214ea1912af128f4d79acd4fc42707fc4b0088cc
DESKTOP-52351T8\Admin <yojhf@naver.com>
2025-01-16 14:12:01 +0900

    no message

--- Assets/_Develop/Seti/Scripts/Utilities/Lecture/Damagable.DamageMessage.cs
+++ Assets/_Develop/Seti/Scripts/Utilities/Lecture/Damagable.DamageMessage.cs
@@ -9,7 +9,7 @@ namespace Seti
         public struct DamageMessage
         {
             public MonoBehaviour damager;
-            public int amount;
+            public float amount;
 
             public Vector3 direction;
             public Vector3 damageSource;
