945153da88c2e6607a960ca191ae916989496786
xianium92 <xianium92@gmail.com>
2025-02-07 16:34:45 +0900

    1

--- Assets/_Develop/Seti/__SO/PlayerStatsObject.cs
+++ Assets/_Develop/Seti/__SO/PlayerStatsObject.cs
@@ -5,6 +5,10 @@ namespace Seti
     [CreateAssetMenu(fileName = "PlayerStatsObject", menuName = "Scriptable Objects/PlayerStatsObject")]
     public class PlayerStatsObject : ScriptableObject
     {
-
+        public float hp = 100f;
+        public float atk = 5f;
+        public float def = 0f;
+        public float moveSpeed = 5f;
+        public float atkSpeed = 1f;
     }
 }
