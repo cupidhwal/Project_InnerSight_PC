b34ad44f815f311142051ee1fef97c97449a77e7
DESKTOP-0U9M598\M <yooniquegame@gmail.com>
2025-02-19 18:01:51 +0900

    1. 메인메뉴 씬 추가 2. 영준님 폴더 안프리펩 수정

---
+++ Assets/_Develop/_Yoon/02_Scripts/UI/MainMenu.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Noah;
+
+namespace Yoon
+{
+    public class MainMenu : MonoBehaviour
+    {
+        #region Variables
+        public SceneFade fader;
+        [SerializeField] private string loadToScene = "PlayScene";        //playScene 불러오기
+
+        public GameObject mainMenuUI;
+        public GameObject creditUI;
+        public GameObject continueButton;
+
+        #endregion
+
+        private void Start()
+        {
+            //게임 데이터 초기화
+            InitGameData();
+            //저장된 씬이 있으면
+
+            //if(PlayerStats.Instance.SceneNumber > 0)
+            //{
+            //    continueButton.SetActive(true);
+            //}
+
+            //씬 페이드인 효과
+            //fader.FromFade();                 //TODO : fader 스크립트 확인
+
+
+        }
+
+        private void InitGameData()
+        {
+            //게임 플레이 데이터 로드
+            //PlayData playData = SaveLoad.LoadData();
+            //PlayerStats.Instance.PlayerStatInit(playData);
+        }
+
+        public void NewGame()
+        {
+            Debug.Log("NewGame");
+
+            //게임 데이터 초기화
+            //PlayerStats.Instance.PlayerStatInit(null);
+
+            //fader.FadeTo(loadToScene);
+        }
+
+        public void ContinueGame()
+        {
+            Debug.Log("ContinueGame");
+            
+            //fader.FadeTo(PlayerStats.Instance.SceneNumber);
+        }
+
+
+        public void Credits()
+        {
+            ShowCredit();
+        }
+
+        public void QuitGame()
+        {
+            Debug.Log("Quit Game");
+            Application.Quit();
+        }
+
+        //크레딧 UI 실행
+        private void ShowCredit()
+        {
+            Debug.Log("ShowCredit");            //
+
+            //mainMenuUI.SetActive(false);
+            //creditUI.SetActive(true);
+        }
+    }
+}
