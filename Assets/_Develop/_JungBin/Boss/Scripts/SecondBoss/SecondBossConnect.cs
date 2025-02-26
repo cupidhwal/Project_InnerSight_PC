b96713f843622613f243f5f0bf9a39a127cc28d3
jb000406 <anvill1234@gmail.com>
2025-02-26 15:43:31 +0900

    11

---
+++ Assets/_Develop/_JungBin/Boss/Scripts/SecondBoss/SecondBossConnect.cs
@@ -0,0 +1,24 @@
+using JungBin;
+using UnityEngine;
+
+public class SecondBossConnect : MonoBehaviour
+{
+    [SerializeField] private BossStageManager bossStageManager;
+    [SerializeField] private GameObject Phase1;
+    [SerializeField] private GameObject Phase2;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        bossStageManager.EnterBossStage(0);
+    }
+
+    public void PhaseChange()
+    {
+        Phase2.transform.position = Phase1.transform.position;
+        Phase2.SetActive(true);
+        Debug.Log("Phase2 보스 등장");
+    }
+
+
+}
