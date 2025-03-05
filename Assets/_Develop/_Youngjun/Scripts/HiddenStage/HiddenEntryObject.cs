f68c50fcc6de641f93ad6c0c6f87c7e6c1d63230
DESKTOP-0U9M598\M <yojhf@naver.com>
2025-03-05 15:27:13 +0900

    no message

--- Assets/_Develop/_Youngjun/Scripts/HiddenStage/HiddenEntryObject.cs
+++ Assets/_Develop/_Youngjun/Scripts/HiddenStage/HiddenEntryObject.cs
@@ -6,6 +6,7 @@ namespace Noah
     public class HiddenEntryObject : MonoBehaviour
     {
         public float riseSpeed = 2f; // 상승 속도 설정
+        public float maxUp;
         public GameObject hiddenParitcle;
         
         private Vector3 paritclePos;
@@ -19,14 +20,14 @@ namespace Noah
         {
             GameObject paritcle = Instantiate(hiddenParitcle, new Vector3(transform.position.x, 0f, transform.position.z), Quaternion.identity);
 
-            while (transform.position.y < 0f)
+            while (transform.position.y < maxUp)
             {
                 transform.position += Vector3.up * riseSpeed * Time.deltaTime;
                 yield return null; // 다음 프레임까지 대기
             }
 
             // y = 0 보정
-            transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+            transform.position = new Vector3(transform.position.x, maxUp, transform.position.z);
 
             transform.GetComponent<Collider>().enabled = true;
             transform.GetChild(0).gameObject.SetActive(true);
