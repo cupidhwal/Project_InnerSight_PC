6968516c54f3b3725d33aebdd0e262d155fbaa44
DESKTOP-0U9M598\M <yojhf@naver.com>
2025-03-12 11:05:10 +0900

    1. Stage, Skill Trigger object Add ActionUI

--- Assets/_Develop/_Youngjun/Scripts/Object/RandomSkill.cs
+++ Assets/_Develop/_Youngjun/Scripts/Object/RandomSkill.cs
@@ -6,22 +6,57 @@ namespace Noah
     {
         //ParticleSystem[] particleSystems;
 
+        private string actionUI_Text = "";
+        private bool isContact = false;
+
         private void Start()
         {
             ObjectFadeSystem.Instance.ObjectFadeIn_Particle(transform);
+        }
+
+        private void Update()
+        {
+            if (isContact)
+            {
+                actionUI_Text = "마기흡수";
+
+                ActionUIManager.Instance.EnableActionUI(actionUI_Text);
+
+                if (Input.GetKeyDown(KeyCode.G))
+                {
+                    GetRandomSkill();
+                }
+            }
+            else
+            {
+                ActionUIManager.Instance.DisableActionUI();
+            }
+        }
+
+        void GetRandomSkill()
+        {
+            transform.GetComponent<Collider>().enabled = false;
+
+            UIManager.Instance.skillSelectUI.SetActive(true);
+            InGameUI_Skill.instance.GetRandomSkill();
+            Time.timeScale = 0f;
+
+            Destroy(gameObject);
         }
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
-                transform.GetComponent<Collider>().enabled = false;
+                isContact = true;
+            }
+        }
 
-                UIManager.Instance.skillSelectUI.SetActive(true);
-                InGameUI_Skill.instance.GetRandomSkill();
-                Time.timeScale = 0f;
-
-                Destroy(gameObject);
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                isContact = false;
             }
         }
 
--- Assets/_Develop/_Youngjun/Scripts/Object/SkillReinforce.cs
+++ Assets/_Develop/_Youngjun/Scripts/Object/SkillReinforce.cs
@@ -4,31 +4,66 @@ namespace Noah
 {
     public class SkillReinforce : MonoBehaviour
     {
+        private string actionUI_Text = "";
+        private bool isContact = false;
+
         private void Start()
         {
             ObjectFadeSystem.Instance.ObjectFadeIn_Particle(transform);
+        }
+
+        private void Update()
+        {
+            if (isContact)
+            {
+                actionUI_Text = "마기강화";
+
+                ActionUIManager.Instance.EnableActionUI(actionUI_Text);
+
+                if (Input.GetKeyDown(KeyCode.G))
+                {
+                    GetSkillReinforce();
+                }
+            }
+            else
+            {
+                ActionUIManager.Instance.DisableActionUI();
+            }
+        }
+
+        void GetSkillReinforce()
+        {
+            transform.GetComponent<Collider>().enabled = false;
+
+            UIManager.Instance.skillReinforce.SetActive(true);
+
+            UIManager.Instance.skillReinforce.GetComponent<InGameUI_SkillReinforce>().SetSkill();
+
+            Time.timeScale = 0f;
+
+            if (HiddenStageManager.Instance != null)
+            {
+                HiddenStageManager.Instance.SelectReinforce();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
         }
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
-                transform.GetComponent<Collider>().enabled = false;
+                isContact = true;
+            }
+        }
 
-                UIManager.Instance.skillReinforce.SetActive(true);
-
-                UIManager.Instance.skillReinforce.GetComponent<InGameUI_SkillReinforce>().SetSkill();
-
-                Time.timeScale = 0f;
-
-                if (HiddenStageManager.Instance != null)
-                {
-                    HiddenStageManager.Instance.SelectReinforce();
-                }
-                else
-                {
-                    Destroy(gameObject);
-                }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                isContact = false;
             }
         }
     }
